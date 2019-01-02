b921f7dbcf08addb4209992a9bae11fe8bf4e7c0
Mateusaa <46310153+D3sertPower@users.noreply.github.com>
2019-01-02 03:52:40 -0200

    adgadg
    asdgasdg

---
+++ bot.js
@@ -0,0 +1,48 @@
+const discord = require("discord.js");
+const fs = require("fs");
+const config = require("./config.json");
+const bot = new discord.Client({disableEveryone: true});
+
+// When bot ready
+bot.on("ready", async () => {
+  console.log(`Bot foi iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`); 
+  client.user.setPresence({ game: {name: 'DG GAMERSZ', type: 1, url: 'https://www.twitch.tv/dggamersz'} });
+});
+
+// Load commands
+bot.commands = new discord.Collection();
+fs.readdir("./commands/", (err, files) => {
+  if (err) console.error(err);
+  let jsfiles = files.filter(f => f.split(".").pop() === "js");
+
+  if (jsfiles.length <= 0) return console.log("There are no commands to load...");
+
+  console.log(`Loading ${jsfiles.length} commands...`);
+  jsfiles.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`${i + 1}: ${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+// Message event
+bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+  if(!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+});
+
+bot.on("guildCreate", guild => {
+  console.log(`O bot entrou nos servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+bot.on("guildDelete", guild => {
+  console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+});
+
+bot.login(config.token);
